56da63afd629225326c6c1086c33285b4aee4f4c
DELTHA-TECH <techdeltha@gmail.com>
2025-03-22 09:32:28 -0500

    Imágenes de modelos, logos, y servicio técnico añadidas

--- copiar_imagenes.sh
+++ copiar_imagenes.sh
@@ -20,3 +20,42 @@ cp -r ~/storage/shared/Pictures/Gallery/owner/'logo DELTHA-TECH'/. images/Logos/
 cp -r ~/storage/shared/Pictures/Gallery/owner/'logo de marcas '/. images/Logos/
 
 echo "¡Imágenes copiadas con éxito!"
+#!/bin/bash
+
+echo "[+] Iniciando copia automática de imágenes desde la galería..."
+
+# Directorios origen en tu almacenamiento interno
+ORIGEN=~/storage/shared/Pictures/Gallery/owner
+
+# Directorios destino en el proyecto
+DESTINO=./imagenes
+
+# Función para copiar imágenes
+copiar_imagenes() {
+    carpeta_origen="$1"
+    carpeta_destino="$2"
+
+    mkdir -p "$DESTINO/$carpeta_destino"
+
+    if cp -u "$ORIGEN/$carpeta_origen/"* "$DESTINO/$carpeta_destino/"; then
+        echo "[+] Imágenes copiadas de '$carpeta_origen' a '$carpeta_destino'"
+    else
+        echo "[!] Error al copiar imágenes desde '$carpeta_origen'"
+    fi
+}
+
+# Copiando modelos por marca
+copiar_imagenes "modelós de iPhone" "iPhone"
+copiar_imagenes "modelós de Xiaomi" "Xiaomi"
+copiar_imagenes "modelós de honor" "Honor"
+copiar_imagenes "modelós de Samsung" "Samsung"
+
+# Copiando logos
+copiar_imagenes "logo de marcas" "logos_marcas"
+copiar_imagenes "logo DELTHA-TECH" "logos_empresa"
+
+# Copiando servicio técnico y análisis forense
+copiar_imagenes "servicio técnico" "servicio_tecnico"
+copiar_imagenes "análisis forence " "analisis_forense"
+
+echo "[✓] Copia completada. Verifica las imágenes en la carpeta ./imagenes/"
